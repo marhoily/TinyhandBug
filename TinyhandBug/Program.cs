8ecc856e9de4846169aeb1ea601dfdee37e88ce7
Ilya <marhoily@hotmail.com>
2021-11-17 19:15:01 +0300

    Add project files.

---
+++ TinyhandBug/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Tinyhand;
+
+namespace TinyhandBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var myClass = new MyClass() { Age = 10, FirstName = "hoge", LastName = "huga", };
+            var b = TinyhandSerializer.Serialize(myClass);
+            var myClass2 = TinyhandSerializer.Deserialize<MyClass>(b);
+
+
+            var myClassRecon = TinyhandSerializer.Reconstruct<MyClass>(); // Create a new instance whose members have default values.
+        }
+    }
+
+    [TinyhandObject] // Annote a [TinyhandObject] attribute.
+    public partial class MyClass // partial class is required for source generator.
+    {
+        // Key attributes take a serialization index (or string name)
+        // The values must be unique and versioning has to be considered as well.
+        [Key(0)]
+        public int Age { get; set; }
+
+        [Key(1)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Key(2)]
+        [DefaultValue("Doe")] // If there is no corresponding data, the default value is set.
+        public string LastName { get; set; } = string.Empty;
+
+        // All fields or properties that should not be serialized must be annotated with [IgnoreMember].
+        [IgnoreMember]
+        public string FullName { get { return FirstName + LastName; } }
+
+        [Key(3)]
+        public List<string> Friends { get; set; } = default!; // Non-null value will be set by TinyhandSerializer.
+
+        [Key(4)]
+        public int[]? Ids { get; set; } // Nullable value will be set null.
+
+        public MyClass()
+        {
+            // this.Reconstruct(TinyhandSerializerOptions.Standard); // optional: Call Reconstruct() to actually create instances of members.
+        }
+    }
+}
